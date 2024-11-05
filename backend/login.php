9f41edfb1f9577a1a94ea825928324ab0329df2b
SAdeline <support@assurazot.com>
2024-11-05 13:19:06 +0400

    username > lastname

--- backend/login.php
+++ backend/login.php
@@ -4,21 +4,20 @@ session_start();
 include("./bdd.php");
 
 // Gestion de l'authentification
-if (isset($_POST['username']) && isset($_POST['password'])) {
-    $username = $_POST['username'];
+if (isset($_POST['lastname']) && isset($_POST['password'])) {
+    $lastname = $_POST['lastname'];
     $password = $_POST['password'];
 
-    $check = $pdo->prepare('SELECT * FROM users WHERE username = :username AND password = :password');
-    $check->bindParam(':username', $username, PDO::PARAM_STR);
+    $check = $pdo->prepare('SELECT * FROM users WHERE lastname = :lastname AND password = :password');
+    $check->bindParam(':lastname', $lastname, PDO::PARAM_STR);
     $check->bindParam(':password', $password, PDO::PARAM_STR);
     $check->execute();
     // Récupérer tous les résultats sous forme de tableau associatif
     $users = $check->fetchAll(PDO::FETCH_ASSOC);
 
-    var_dump($users);
-    if (isset($users[0]) && $users[0]["password"] === $password && $users[0]['username'] === $username) {
+    if (isset($users[0]) && $users[0]["password"] === $password && $users[0]['lastname'] === $lastname) {
         $_SESSION['logged_in'] = true;
-        $_SESSION['username'] = $username;
+        $_SESSION['lastname'] = $lastname;
         header("Location: ./../upload.php");
         exit();
 
--- login.php
+++ login.php
@@ -17,7 +17,7 @@ if (!isset($_SESSION['logged_in']) || !$_SESSION['logged_in']) {
 
     <form method="post" action="./backend/login.php" class="card my-2 p-2 w-50 d-flex">
         <h2 class="text-center">Se connecter</h2>
-        Nom d'utilisateur : <input type="text" class="form-control" name="username"><br>
+        Nom d'utilisateur : <input type="text" class="form-control" name="lastnanme"><br>
         Mot de passe : <input type="password" class="form-control" name="password"><br>
         <input type="submit" class="btn btn-primary" value="Se connecter">
         <a class="text-center" href="./signup.php">s'inscrire</a>
